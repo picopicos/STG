76e279a73d1e05db3f9c296ac7aac51e1e66ec6a
picopicos <picopicoico@gmail.com>
2021-01-28 09:04:38 +0900

    add キー入力に応じて移動
    上下左右キーでspeedの値の分、自機が移動できるようにした

--- data/scripts/entity.js
+++ data/scripts/entity.js
@@ -19,6 +19,7 @@ class Entity{
         this.width = width;
         this.height = height;
         this.life = life;
+        this.speed = 1.5;
 
         this.image = new Image();
         this.ready = false;
@@ -51,8 +52,16 @@ class Entity{
     update(){
         // TODO:移動処理
         if(window.Is_key_down.key_ArrowLeft === true){
-
-            this.position.x++;
+            this.position.x -= this.speed;
+        }
+        if(window.Is_key_down.key_ArrowRight === true){
+            this.position.x += this.speed;
+        }
+        if(window.Is_key_down.key_ArrowUp === true){
+            this.position.y -= this.speed;
+        }
+        if(window.Is_key_down.key_ArrowDown === true){
+            this.position.y += this.speed;
         }
         this.draw();
     }
